212a0507d52346fa7ecc9cedc00fe0fdbd8eb365
evans129 <104578248+evans129@users.noreply.github.com>
2022-05-14 01:15:56 +0530

    initial commit

---
+++ diabetes_predictor_ui.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import pickle
+
+
+# In[4]:
+
+
+filename="trained_model.sav"
+load_model=pickle.load(open(filename,"rb"))
+
+
+# In[ ]:
+
+
+def diabetes_prediction(input_data):
+ print("Given data")
+ print(input_data)
+ numpy_data=np.array(input_data)
+ data=numpy_data.reshape(1,-1)
+ #std_data=scaler.transform(data)
+ prediction=load_model.predict(data)
+ print("Outcome predicted: ",end="")
+ print(prediction[0])
+ if(prediction[0]==1):
+    return "The patient is having diabetes"
+ else:
+    return "The patient is non diabetic"
+    
+
+
+# using streamlit library
+
+# In[5]:
+
+
+import streamlit as st
+
+
+# In[7]:
+
+
+def main():
+    st.title("Diabetes_predictor")
+#getting inputs from the user
+    Pregnancies=st.text_input("No of Pregnancies")
+    Glucose=st.text_input("Glucose level")
+    BloodPressure=st.text_input("Bloodpressure level")
+    SkinThickness=st.text_input("SkinThickness value")
+    Insulin=st.text_input("Insulin level")
+    BMI=st.text_input("BMI value")
+    DiabetesPedigreeFunction=st.text_input("DiabetesPredigreeFunction")
+    Age=st.text_input("Age of the person")
+#code for prediction 
+    diagnosis=""
+    if(st.button("Diabetes_test_result")):
+        diagnosis=diabetes_prediction([Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age])
+    st.success(diagnosis)
+if __name__ == '__main__':
+    main()
+
+
+# In[ ]:
+
+
+
+
